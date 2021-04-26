58d5b9921c0f9d23f523fe064e405d5c9eb36efc
MohamadHammash <77494189+MohamadHammash@users.noreply.github.com>
2021-04-26 09:52:23 +0200

    init

---
+++ Lms.MVC/Lms.Core/Entites/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entites
+{
+    class Student : User
+    {
+
+        public Course Course { get; set; }
+
+    }
+}
