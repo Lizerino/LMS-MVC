ea007961a4a2cb0c84a41862ee1f794922e870f3
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-04-29 16:22:40 +0200

    forgot to add viewmodel

---
+++ Lms.MVC/Lms.UI/Models/ViewModels/CourseListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Lms.MVC.Core.Entities;
+
+namespace Lms.MVC.UI.Models.ViewModels
+{
+    public class CourseListViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        // nav prop        
+        public ICollection<ApplicationUser> Users { get; set; }
+        public ICollection<Module> Modules { get; set; }
+        public ICollection<Document> Documents { get; set; }
+
+    }
+}
+
