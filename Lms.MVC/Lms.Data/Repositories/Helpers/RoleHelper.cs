6b5cb864a793b1e606145c4c7bf02f4f8e590164
MohamadHammash <77494189+MohamadHammash@users.noreply.github.com>
2021-04-30 15:25:40 +0200

    add/edit done including roles

---
+++ Lms.MVC/Lms.Data/Repositories/Helpers/RoleHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.MVC.Data.Repositories.Helpers
+{
+    public static class RoleHelper
+    {
+        public static string Student => "Student";
+        public static string Admin => "Admin";
+        public static string Teacher => "Teacher";
+
+        //public static string[] AdminTeacher = { "Admin", "Teacher" };
+
+        //public static string[] AdminStudent = { "Admin", "Student" };
+
+    }
+}
