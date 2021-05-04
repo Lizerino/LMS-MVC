9eb549e1aae6733591f62685d11e1d2171125d7e
K-J <kj.ortigoza@gmail.com>
2021-05-04 16:08:24 +0200

    just initialstuff

---
+++ Lms.Tests/Controllers/MVCUIControlersTests.cs
@@ -0,0 +1,20 @@
+﻿using Lms.MVC.UI.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Tests.Controllers
+{
+    [TestClass]
+    public class MVCUIControlersTests
+    {
+        [TestInitialize]
+        public void SetUp()
+        {
+
+        }
+    }
+}
