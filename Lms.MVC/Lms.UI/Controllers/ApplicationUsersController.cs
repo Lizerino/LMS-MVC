f0eb6f0036705481759b00c27c0bd21341c9aa35
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-05-04 15:39:44 +0200

    redesigned admin landing page to not include list of modules or activities

--- Lms.MVC/Lms.UI/Controllers/ApplicationUsersController.cs
+++ Lms.MVC/Lms.UI/Controllers/ApplicationUsersController.cs
@@ -32,6 +32,7 @@ namespace Lms.MVC.UI.Controllers
             this.uoW = uoW;
             this.mapper = mapper;
         }
+               
 
         // GET: ApplicationUsersController
         public async Task<IActionResult> Index(string search, string sortOrder, int page)
@@ -47,7 +48,7 @@ namespace Lms.MVC.UI.Controllers
             var model = mapper.Map<IEnumerable<ListApplicationUsersViewModel>>(users);
             if (!String.IsNullOrWhiteSpace(search))
             {
-                model = model.Where(u => u.Name.ToLower().StartsWith(search.ToLower()) || u.Email.ToLower().Contains(search.ToLower()));
+                model = model.Where(u => u.Name.ToLower().StartsWith(search.ToLower()) || u.Email.ToLower().Contains(search.ToLower()) || u.Role.ToLower().Trim()==search.ToLower());
             }
 
             ViewData["CurrentFilter"] = search;
