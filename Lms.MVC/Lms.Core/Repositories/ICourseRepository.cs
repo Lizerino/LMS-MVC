57df3885ec8bc95d44495ae3d943e916e3ef309a
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-05-06 09:43:32 +0200

    Rewrote coursecontroller to user unit of work

--- Lms.MVC/Lms.Core/Repositories/ICourseRepository.cs
+++ Lms.MVC/Lms.Core/Repositories/ICourseRepository.cs
@@ -1,14 +1,18 @@
-﻿using Lms.MVC.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Lms.MVC.Core.Entities;
 
 namespace Lms.MVC.Core.Repositories
 {
     public interface ICourseRepository
     {
-        
+        Task AddAsync<T>(T added);
+        Task<IEnumerable<Course>> GetAllCoursesAsync(bool includeModules);
+        Task<Course> GetCourseAsync(int? id);
+        void Remove(Course removed);
+        void Remove<T>(T removed);
+        Task<bool> SaveAsync();
+        public async Task<bool> CourseExists(int id)
     }
 }
