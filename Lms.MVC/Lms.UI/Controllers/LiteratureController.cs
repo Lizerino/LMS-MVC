b7fae96a96110863d19409b8d0d421b7797f9d8c
anita <anita.marjatta.linden@gmail.com>
2021-05-06 16:00:04 +0200

    General search of literature by author, title, and subject works

---
+++ Lms.MVC/Lms.UI/Controllers/LiteratureController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Lms.MVC.UI.Controllers
+{
+    public class LiteratureController : Controller
+    {
+        string BaseUrl = "https://localhost:44302/";
+        // GET: Products
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = "https://localhost:44302/api/Literatures";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    var table = Newtonsoft.Json.JsonConvert.DeserializeObject<System.Data.DataTable>(data);
+                }
+            }
+            return View();
+
+        }
+    }
+
+}
