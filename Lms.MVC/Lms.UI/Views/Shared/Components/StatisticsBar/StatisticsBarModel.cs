3f93506b1496fc4d225ab534af8d092625a2abd6
MohamadHammash <77494189+MohamadHammash@users.noreply.github.com>
2021-05-13 18:55:55 +0200

    willBeBack

---
+++ Lms.MVC/Lms.UI/Views/Shared/Components/StatisticsBar/StatisticsBarModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.MVC.UI.Views.Shared.Components.StatisticsBar
+{
+    public class StatisticsBarModel
+    {
+        public int MyProperty { get; set; }
+    }
+}
