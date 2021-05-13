55265bfadcd3e3a7c80bf8cb889e913651f9c6c4
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-05-13 19:22:42 +0200

    added test project

---
+++ LMS.MVC.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+using System;
+
+using Xunit;
+
+namespace LMS.MVC.Test
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void TeacherRedirectToCoursesIndex()
+        {
+
+        }
+
+        [Fact]
+        public void AdminRedirectToAdminOverview()
+        {
+
+        }
+
+        [Fact]
+        public void StudentRedirectToModulesIndex()
+        {
+
+        }
+
+        [Fact]
+        public void PrivacyDisplaysPrivacyPolicyView()
+        {
+
+        }
+    }
+}
