181f4f804f01acc46afcbd78fbe34a7edc44a612
Stefan Karlsson <stefan.lg.karlsson@gmail.com>
2021-05-15 11:00:40 +0200

    added more users courses etc to seed to test pagination and edge cases

--- Lms.MVC/Lms.Data/Data/SeedData.cs
+++ Lms.MVC/Lms.Data/Data/SeedData.cs
@@ -43,11 +43,11 @@ namespace Lms.MVC.Data.Data
             // new Random(12345);
             Randomizer.Seed = new Random();
 
-            NumberOfCourses = 5;
-            NumberOfModulesPerCourse = 3;
-            NumberOfActivititesPerModule = 3;
-            NumberOfStudentsPerClass = 10;
-            NumberOfTeachersPerClass = 1;
+            NumberOfCourses = 11;
+            NumberOfModulesPerCourse = 11;
+            NumberOfActivititesPerModule = 11;
+            NumberOfStudentsPerClass = 11;
+            NumberOfTeachersPerClass = 2;
 
             NumberOfModules = NumberOfCourses * NumberOfModulesPerCourse;
             NumberOfActivities = NumberOfModules * NumberOfActivititesPerModule;
